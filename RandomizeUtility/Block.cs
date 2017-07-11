0687145094cc0eafa266ada373a75467ebd36921
r sugawara <ryo.sugawara.1224@gmail.com>
2017-07-11 17:23:06 +0900

    RandomizeUtilityのコアロジック修正のため 一旦まずブロック分割のアルゴリズムを完成させる事にした。 Blockクラスとそのテストプロジェクトを追加。

---
+++ RandomizeUtility/Block.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomizeUtility
+{
+	public class Block
+	{
+		public int Begin { get; private set; }
+		public int End   { get; private set; }
+		public int Count { get { return End - Begin + 1; } }
+
+		public Block(int begin, int end)
+		{
+			this.Begin = Math.Min( begin, end );
+			this.End   = Math.Max( begin, end );
+		}
+
+		public IEnumerable<int> AsIndexes()
+		{
+			for ( int i = this.Begin; i < this.Count; i++ )
+			{
+				yield return i;
+			}
+		}
+
+		public override string ToString()
+		{
+			return $"{this.Begin}-{this.End}({this.Count})";
+		}
+
+		public static List<Block> As( int size, int n )
+		{
+			List<Block> blocks = new List<Block>();
+			
+			decimal seeksize = (decimal)size / (decimal)n;
+
+			// N回のブロック生成を行う。
+			decimal index = 0m;
+			for ( int i = 0; i < n; i++ )
+			{
+				int begin = (int)Math.Ceiling(index);
+
+				index += seeksize;
+
+				int end = (int)Math.Ceiling(index)-1;
+
+				Block block = new Block(begin, end);
+				blocks.Add( block );
+			}
+
+
+			return blocks;
+		}
+	}
+}
---
+++ RandomizeUtilityTest/BlockTest.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RandomizeUtility;
+using System.Reflection;
+
+namespace RandomizeUtilityTest
+{
+	// xUnit 的なスマートなアレにしたいなぁ、Assertクラスはイケてないから何か考えたい。できれば追加依存無しで。。。
+
+	[TestClass]
+	public class BlockTest
+	{
+		[TestMethod]
+		public void Case10div5_等分割するパターン()
+		{
+			var test = new {
+				S = 10,
+				N = 5,
+			};
+
+			var block = Block.As( test.S, test.N );
+			
+			Assert.AreEqual( test.N, block.Count );
+
+			foreach ( var i in block )
+			{
+				Console.WriteLine( MethodBase.GetCurrentMethod().ToString() + " : " + i );
+			}
+		}
+		
+		[TestMethod]
+		public void Case10div3_分割数半分以下で余り有り()
+		{
+			var test = new {
+				S = 10,
+				N = 3,
+			};
+
+			var block = Block.As( test.S, test.N );
+			
+			Assert.AreEqual( test.N, block.Count );
+
+			foreach ( var i in block )
+			{
+				Console.WriteLine( MethodBase.GetCurrentMethod().ToString() + " : " + i );
+			}
+		}
+		
+		[TestMethod]
+		public void Case10div7_分割数半分以上で余り有り()
+		{
+			
+			var test = new {
+				S = 10,
+				N = 7,
+			};
+
+			var block = Block.As( test.S, test.N );
+			
+			Assert.AreEqual( test.N, block.Count );
+
+			foreach ( var i in block )
+			{
+				Console.WriteLine( MethodBase.GetCurrentMethod().ToString() + " : " + i );
+			}
+		}
+
+
+	}
+}
