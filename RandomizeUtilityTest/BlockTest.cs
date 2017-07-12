c73c9474d2367219855fbe44d223a3d9bd2b2c31
r sugawara <ryo.sugawara.1224@gmail.com>
2017-07-12 14:22:24 +0900

    Blockのテストプロジェクトにテストを追加。 ・ブロック分割の内容をテストするDetailテストクラスを追加。 ・その他、コード整理の為リファクタリング。

--- RandomizeUtilityTest/BlockTest.cs
+++ RandomizeUtilityTest/BlockTest.cs
@@ -9,64 +9,121 @@ namespace RandomizeUtilityTest
 {
 	// xUnit 的なスマートなアレにしたいなぁ、Assertクラスはイケてないから何か考えたい。できれば追加依存無しで。。。
 
+	/// <summary>
+	/// ブロックの分割数だけざっくりテスト。内容に関してはDetailの方で検証する。
+	/// </summary>
 	[TestClass]
 	public class BlockTest
 	{
 		[TestMethod]
-		public void Case10div5_等分割するパターン()
+		public void CASE0a_10div1_むしろ分割しない()
+		{
+			var test = new {
+				S = 10,
+				N = 1,
+			};
+
+			var blocks = Block.As( test.S, test.N );
+			
+			Assert.AreEqual( test.N, blocks.Count );
+
+			foreach ( var i in blocks )
+			{
+				Console.WriteLine( MethodBase.GetCurrentMethod().ToString() + " : " + i );
+			}
+		}
+		[TestMethod]
+		public void CASE0b_10div10_むしろ全部分割する()
+		{
+			var test = new {
+				S = 10,
+				N = 10,
+			};
+
+			var blocks = Block.As( test.S, test.N );
+			
+			Assert.AreEqual( test.N, blocks.Count );
+
+			foreach ( var i in blocks )
+			{
+				Console.WriteLine( MethodBase.GetCurrentMethod().ToString() + " : " + i );
+			}
+		}
+
+
+		
+		[TestMethod]
+		public void CASE1a_10div5_等分割するパターン1()
 		{
 			var test = new {
 				S = 10,
 				N = 5,
 			};
 
-			var block = Block.As( test.S, test.N );
+			var blocks = Block.As( test.S, test.N );
 			
-			Assert.AreEqual( test.N, block.Count );
+			Assert.AreEqual( test.N, blocks.Count );
 
-			foreach ( var i in block )
+			foreach ( var i in blocks )
 			{
 				Console.WriteLine( MethodBase.GetCurrentMethod().ToString() + " : " + i );
 			}
 		}
+		[TestMethod]
+		public void CASE1b_10div2_等分割するパターン2()
+		{
+			var test = new {
+				S = 10,
+				N = 2,
+			};
+
+			var blocks = Block.As( test.S, test.N );
+			
+			Assert.AreEqual( test.N, blocks.Count );
+
+			foreach ( var i in blocks )
+			{
+				Console.WriteLine( MethodBase.GetCurrentMethod().ToString() + " : " + i );
+			}
+		}
+
 		
 		[TestMethod]
-		public void Case10div3_分割数半分以下で余り有り()
+		public void CASE2a_10div3_分割数半分以下で余り有り()
 		{
 			var test = new {
 				S = 10,
 				N = 3,
 			};
 
-			var block = Block.As( test.S, test.N );
+			var blocks = Block.As( test.S, test.N );
 			
-			Assert.AreEqual( test.N, block.Count );
+			Assert.AreEqual( test.N, blocks.Count );
 
-			foreach ( var i in block )
+			foreach ( var i in blocks )
 			{
 				Console.WriteLine( MethodBase.GetCurrentMethod().ToString() + " : " + i );
 			}
 		}
 		
 		[TestMethod]
-		public void Case10div7_分割数半分以上で余り有り()
+		public void CASE2b_10div7_分割数半分以上で余り有り()
 		{
-			
 			var test = new {
 				S = 10,
 				N = 7,
 			};
 
-			var block = Block.As( test.S, test.N );
+			var blocks = Block.As( test.S, test.N );
 			
-			Assert.AreEqual( test.N, block.Count );
+			Assert.AreEqual( test.N, blocks.Count );
 
-			foreach ( var i in block )
+			foreach ( var i in blocks )
 			{
 				Console.WriteLine( MethodBase.GetCurrentMethod().ToString() + " : " + i );
 			}
 		}
 
-
+		// テスト結果の表示順を弄れなかったから仕方なく CASE9x でメソッド名を並べてやったが、、、。
 	}
 }
